696eedda198ba02ee43b40f08a9abdec2c10b27a
Vishalpateldodla <117738669+Vishalpateldodla@users.noreply.github.com>
2023-02-20 11:56:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,6 +4,13 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
+  const prices = document.querySelectorAll(".price");
+  let total = 0;
+  for (let i = 0; i < prices.length; i++) {
+    total += Number(prices[i].textContent);
+  }
+  const totalCell = document.getElementById("total");
+  totalCell.textContent = `Total Price: ${total}`;
   
 };
 
